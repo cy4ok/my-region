971f65983b181ba21ad236ceffb5e95daec11f23
cy4ok <cy4ok86@gmail.com>
2021-03-13 13:48:11 +0300

    Скелет главной страницы
    Закончил скелет главной страницы.

--- client/src/page/MainPage.js
+++ client/src/page/MainPage.js
@@ -7,6 +7,9 @@ import star from  "../images/icons/star.png"
 import clock from  "../images/icons/clock.png"
 import LogoTrack from  "../images/icons/LogoTrack.png"
 import line from "../images/icons/line.png"
+import arrowLeft from "../images/icons/arrowLeft.png"
+import arrowRight from "../images/icons/arrowRight.png"
+import social from "../images/icons/social.png"
 
 const mainPage = () => {
     return (
@@ -69,10 +72,61 @@ const mainPage = () => {
                     </div>
                 </div>
             </div>
-            <div class="w-auto h-12 bg-secondary">
+            <div class="w-auto bg-secondary">
                 <span>Отзывы</span>
                 <div>
-
+                    <img src={arrowLeft} alt="arrowLeft" />
+                    <div>
+                        <span>Зимний тур в Териберку</span>
+                        <span>17.08.21-21.08.21</span>
+                        <span>Матвей Миронов</span>
+                        <span>Отдохнул отлично, перезагрузился. Тур покупал в последний момент, чтобы убежать на юг из холодной Москвы. Про Египет не знал ничего кроме пирамид. Луксор с его древними храмами вообще оказался для меня полным сюрпризом. </span>
+                        <span>Сложность: </span><span>Тяжело</span>
+                        <span>Инструктор: </span><span>Понравился</span>
+                        <span>Общая оценка: </span><span>Хорошо</span>
+                    </div>
+                    <div>
+                        <span>Зимний тур в Териберку</span>
+                        <span>17.08.21-21.08.21</span>
+                        <span>Матвей Миронов</span>
+                        <span>Отдохнул отлично, перезагрузился. Тур покупал в последний момент, чтобы убежать на юг из холодной Москвы. Про Египет не знал ничего кроме пирамид. Луксор с его древними храмами вообще оказался для меня полным сюрпризом. </span>
+                        <span>Сложность: </span><span>Тяжело</span>
+                        <span>Инструктор: </span><span>Понравился</span>
+                        <span>Общая оценка: </span><span>Хорошо</span>
+                    </div>
+                    <img src={arrowRight} alt="arrowRight" />
+                </div>
+            </div>
+            <div class="w-auto bg-primary">
+                <div>
+                    <img src={MainLogo} alt="mainLogo" />
+                    <span>г.Москва, ул.Родионова 13, офис 156</span>
+                    <span>+7 (982) 234 45 28</span>
+                </div>
+                <div>
+                    <span>Ссылки</span>
+                    <span>Маршруты</span>
+                    <span>Обучение</span>
+                    <span>Личный кабинет</span>
+                </div>
+                <div>
+                    <span>Организациям</span>
+                    <span>ООПТ</span>
+                    <span>Тарифы</span>
+                    <span>Тарифы</span>
+                </div>
+                <div>
+                    <span>Обратная связь</span>
+                    <span>Вакансии</span>
+                    <span>Контакты</span>
+                    <span>Обратная связь</span>
+                </div>
+                <div>
+                    <img src={social} alt="social" />
+                    <img src={social} alt="social" />
+                    <img src={social} alt="social" />
+                    <img src={social} alt="social" />
+                    <img src={social} alt="social" />
                 </div>
             </div>
         </div>
